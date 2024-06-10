5f4bdb7a7bc81e64210f77755e696bab806c6245
Debanjan Basu <debanjanbasu2006@gmail.com>
2024-06-10 11:20:49 +1000

    doesn't work, but basic updates

--- src/blob_master.rs
+++ src/blob_master.rs
@@ -1,23 +1,30 @@
+use eyre::{Ok, Result};
+use tokio::io::{BufReader, BufStream};
+
 struct blob<B> {
     inner: B,
-    blob_name: String,
+    input_stream: BufStream<BufReader<B>>,
 }
 
 impl<B> blob<B> {
-    pub fn new(inner: B, blob_name: String) -> Self {
+    pub fn new(inner: B, input_stream: BufStream<BufReader<B>>) -> Self {
         blob {
             inner,
-            blob_name,
+            input_stream,
         }
     }
+
+    pub fn process_stream() -> Result<()> {
+        Ok(())
+    }
 }
+
+// Unit tests for this module
 
 #[cfg(test)]
 mod blob_master {
     use super::*;
 
     #[test]
-    fn create_stream_from_file() {
-        
-    }
+    fn create_stream_from_file() {}
 }
