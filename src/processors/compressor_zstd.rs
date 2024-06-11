9a6b04ff537ab68a357813cb9f086af3959595a8
Debanjan Basu <debanjanbasu2006@gmail.com>
2024-06-11 15:53:20 +1000

    updated with basic compression

--- src/processors/compressor_zstd.rs
+++ src/processors/compressor_zstd.rs
@@ -1,25 +1,31 @@
-use std::io::{BufReader, BufWriter};
+use std::{io::{self, BufReader, BufWriter, Read, Write}, thread::available_parallelism};
 
 use eyre::{Ok, Result};
-use zstd::stream::copy_encode;
 
 #[derive(Debug)]
-pub struct CompressorZstd<R> {
+pub struct CompressorZstd<R: Read, W : Write> {
     level: i32,
     input_buf: BufReader<R>,
     output_buf: BufWriter<W>,
 }
 
-impl<R, W, D> CompressorZstd<R, W, D> {
-    pub fn new(level: i32, input_buf: Reader<R, D>, output_buf: Writer<W, D>) -> Self {
+impl<R: Read, W : Write> CompressorZstd<R, W> {
+    pub fn new(input_buf: BufReader<R>, output_buf: BufWriter<W>, level: i32) -> Self {
         Self {
             level,
             input_buf,
             output_buf,
         }
     }
-    pub async fn process(&mut self) -> Result<()> {
-        copy_encode(self.input_buf.reader().into(), self.output_buf, self.level);
+
+    pub fn process(&mut self) -> Result<()> {
+        // Get the no of threads, for us to parallelize
+        let n_workers = available_parallelism()?.get().try_into()?;
+        let mut encoder = zstd::stream::Encoder::new(&mut self.output_buf, self.level)?;
+        // Enable multithreading
+        encoder.multithread(n_workers)?;
+        // Start the compression
+        io::copy(&mut self.input_buf, &mut encoder.auto_finish())?;
         Ok(())
     }
 }
